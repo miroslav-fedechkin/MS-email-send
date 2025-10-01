0a92747f3682102a42de970971ef54341fd46bd5
Miroslav <fedechkin.miroslav@yandex.ru>
2025-10-01 20:22:20 +0200

    update(config): remove ols fields, add new

--- config.py
+++ config.py
@@ -11,6 +11,9 @@ conf = ConnectionConfig(
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD'),
     MAIL_PORT = os.getenv('MAIL_PORT'),
     MAIL_SERVER = os.getenv('MAIL_SERVER'),
-    MAIL_TLS = os.getenv('MAIL_TLS'),
-    MAIL_SSL = os.getenv('MAIL_SSL'),
+    MAIL_SSL_TLS = os.getenv('MAIL_SSL_TLS'),
+    MAIL_STARTTLS = os.getenv('MAIL_STARTTLS'),
+    MAIL_DEBUG = os.getenv('MAIL_DEBUG'),
+    MAIL_FROM = os.getenv('MAIL_FROM'),
+
 )
