ff9a6dc2d8e13de9c9d9ae6be810a39ac45ab800
Miroslav <fedechkin.miroslav@yandex.ru>
2025-10-01 20:21:59 +0200

    feat(api): add function for send messages

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 from fastapi import FastAPI
+from config import conf
+from fastapi_mail import FastMail, MessageSchema
+from shemas import EmailShema
+from starlette.responses import JSONResponse
 
 app = FastAPI()
+
+
+@app.post('/send_email')
+async def send_email(email: EmailShema):
+    message = MessageSchema(
+        subject='FastAPI-Mail module',
+        recipients=email.model_dump().get('email'),
+        body='message.html',
+        subtype='html'
+    )
+
+
+    fm = FastMail(conf)
+    try:
+        fm.send_message(message)
+        return JSONResponse(status_code=200, content={
+            'status': 'ok'
+        })
+    except Exception as e:
+        print(e)
 
