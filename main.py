e273c5a5442706a2e5384819a685e9a3956f7bee
Miroslav <fedechkin.miroslav@yandex.ru>
2025-10-01 20:43:29 +0200

    update(api): correct read message html template

--- main.py
+++ main.py
@@ -9,17 +9,22 @@ app = FastAPI()
 
 @app.post('/send_email')
 async def send_email(email: EmailShema):
+
+    with open('message_text.html', 'r', encoding='utf-8') as file:
+        html_content = file.read()
+
     message = MessageSchema(
         subject='FastAPI-Mail module',
         recipients=email.model_dump().get('email'),
-        body='message.html',
+        body=html_content,
         subtype='html'
     )
 
+    
 
     fm = FastMail(conf)
     try:
-        fm.send_message(message)
+        await fm.send_message(message)
         return JSONResponse(status_code=200, content={
             'status': 'ok'
         })
